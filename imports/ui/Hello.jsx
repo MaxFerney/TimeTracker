3e8d79bfcc579f949cbdf550bc12ce8653b32609
Michael Walters <waltersm8@winthrop.edu>
2021-02-04 13:48:40 -0500

    Functioning start and stop timer

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -3,15 +3,41 @@ import React, { useState } from 'react';
 export const Hello = () => {
 
     const[counter, setCounter] = useState(Math.floor(Date.now() / 1000 ));
+    const[startTime, setStartTime] = useState("Not Started");
+    const[stopTime, setStopTime] = useState("Not Stopped");
+
+    const[timerStarted, setTimerStarted] = useState("false");
+
 
     setInterval(() => {
       setCounter(Math.floor(Date.now() / 1000 ));
-      console.log(counter);
     }, 1000);
+
+    function getTime() {
+        if(timerStarted == "false") {
+            console.log(timerStarted);
+
+            setStartTime(counter);
+            setStopTime("Not Stopped");
+
+            setTimerStarted("true");
+        } else {
+            console.log(timerStarted);
+
+            setStopTime(counter);
+
+            setTimerStarted("false");
+        }
+    }
 
     return (
         <div>
           <p>Current Unix Time: {counter} </p>
+          <button onClick={getTime}>
+            Get Time
+          </button>
+          <p>Start Time: {startTime}</p>
+          <p>Stop Time: {stopTime}</p>
         </div>
     );
 };
