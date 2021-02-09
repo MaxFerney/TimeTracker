bc720933568444262a0fddf9041544028c55826d
Michael Walters <waltersm8@winthrop.edu>
2021-02-09 14:06:21 -0500

    Working timer

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
+import React, { useState, Component } from 'react';
 import { Info } from './Info.jsx';
 
-var currentTime;
 
 export const App = () => (
   <div>
     <Clock/>
-    <Hello/>
     <Info/>
   </div>
 );
 
+var timerStarted = false;
+var startTime;
+var endTime;
+var currentTime;
+var elapsedTime;
+
+
+function getTime() {
+  if (timerStarted == false) {
+    startTime = currentTime;
+    endTime = "Waiting..."
+    timerStarted = true;
+  } else {
+    endTime = currentTime;
+    timerStarted = false;
+  }
+}
+
 function FormattedDate(props) {
   currentTime = Math.floor(props.date.getTime()/1000);
-  return <h2>It is {currentTime}.</h2>;
+  if(timerStarted == true) {
+    elapsedTime = currentTime - startTime;
+  } else {
+    elapsedTime = endTime - startTime;
+  }
+  return (
+    <div>
+      <h2>It is {currentTime}.</h2>
+      <p>Elapsed Time = {elapsedTime}</p>
+      <p>Start Time: {startTime}</p>
+      <p>Stop time: {endTime}</p>
+      <button onClick={getTime}>
+          Get Time
+      </button>
+    </div>
+  );
 }
 
 class Clock extends React.Component {
@@ -38,7 +68,6 @@ class Clock extends React.Component {
     this.setState({
       date: new Date()
     });
-    console.log("This is a tick");
   }
 
   render() {
