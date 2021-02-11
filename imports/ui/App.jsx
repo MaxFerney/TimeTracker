edb4def6810788d1c6f728bd6e6fa4ee93e5e54f
Max Ferney <maxferney@gmail.com>
2021-02-11 15:12:44 -0500

    database saving added. app needs to run in background for save to work.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,21 +7,49 @@ export const App = () => (
   </div>
 );
 
-var timerStarted = false; //Eventually pull from DB
+//var timerStarted = false; //Eventually pull from DB
 var startTime;
 var endTime;
 var currentTime;
 var timeObject;
+var stateText = "initialize";
+var stateAddText = "none";
 // var timeObjectID;
 var elapsedTime = 0; //Eventually pull from DB
 
 buttonText = "Start"; //Eventually pull from DB
-
+timeObject = TimesCollectionAccess.findOne({is_active:true})
+if (timeObject==undefined){
+    var timerStarted=false;
+    elapsedTime=0;
+    buttonText="Start";
+} else{
+    var timerStarted=true;
+    buttonText="Stop";
+    elapsedTime=1;
+    startTime=timeObject.start_time;
+}
 
 function getTime() {
   if (timerStarted == false) {
+      stateText="timer stopped. inside getTime() timerStarted=true";
+    // timeObject = TimesCollectionAccess.findOne({is_active:true})
+    // if (timeObject==undefined){
+    //
+    // }else{
+    //     console.log("STATE OF TIME OBJECT: "+ timeObject+"isactive: "+timeObject.is_active);
+    // }
+
+    // if (timeObject.is_active == true){
+    //     TimesCollectionAccess.update({_id:timeObject._id},{
+    //         $set:{
+    //         stop_time:currentTime,
+    //         is_active:false,
+    //     }});
+    //
+    // }
     startTime = currentTime;
-    endTime = "Waiting..."
+    endTime = "Waiting...";
     buttonText = "Stop";
     // timeObject = {
     //     start_time: currentTime,
@@ -35,43 +63,76 @@ function getTime() {
     // });
     TimesCollectionAccess.insert({
         start_time: currentTime,
-        stop_time: -1,
-        is_active: "true",
+        stop_time: 0,
+        is_active: true,
     });
     timerStarted = true;
-  } else {
+    timeObject = TimesCollectionAccess.findOne({is_active:true})
+  } else { //TIMER HAS STARTED already
+    stateText="timer started. inside getTime()timerStarted=false";
     endTime = currentTime;
-    timeObject = TimesCollectionAccess.findOne({is_active:"true"})
-    console.log("ENTIRE DB\n\n"+TimesCollectionAccess.find().fetch());
+    timeObject = TimesCollectionAccess.findOne({is_active:true})
+    //console.log("ENTIRE DB\n\n"+TimesCollectionAccess.find().fetch());
     TimesCollectionAccess.update({_id:timeObject._id},{
         $set:{
         stop_time:currentTime,
-        is_active:"false",
+        is_active:false,
     }});
     buttonText = "Start";
     timerStarted = false;
-    console.log(TimesCollectionAccess.find().fetch());
   }
 }
 
 function FormattedDate(props) {
   currentTime = Math.floor(props.date.getTime()/1000);
+  timeObject = TimesCollectionAccess.findOne({is_active:true})
   if(timerStarted == true) {
-    elapsedTime = currentTime - startTime;
-  } else if(endTime) {
-    elapsedTime = endTime - startTime;
-  } else {
-    elapsedTime = 0; //Eventually Pull from DB
-  }
+      stateAddText="timer started true. this is saved state: [";
+    if (timeObject != undefined){
+        stateAddText+="time object defined";
+        startTime = timeObject.start_time;
+        buttonText = "Stop";
+        endTime = "Waiting...";
+        elapsedTime = currentTime - startTime;
+    } else{
+        elapsedTime = currentTime - startTime;
+    }
+  } else if (timerStarted == false) {
+    stateAddText="timer started false. this is default state: [";
+    if (timeObject != undefined){
+        stateAddText+="time object defined";
+        startTime = timeObject.start_time;
+        buttonText = "start";
+        endTime = timeObject.stop_time;
+        elapsedTime = endTime - startTime;
+    } else {
+        elapsedTime = 0;
+    }
+}
+
+  stateText="formatted date. timeObject is undefined: "+(timeObject===undefined).toString()+" | stateAddText: "+stateAddText;
   return (
     <div>
       <h2>It is {currentTime}.</h2>
       <p>Elapsed Time = {elapsedTime}</p>
       <p>Start Time: {startTime}</p>
       <p>Stop time: {endTime}</p>
+      <p>STATE: {stateText}</p>
       <button onClick={getTime}>
           {buttonText}
+
       </button>
+      <h3>stored Times</h3>
+      <div className="times">
+        { TimesCollectionAccess.find().fetch().map((timeItem) =>
+            <div key={timeItem._id} className="time_item">
+                <p>Time ID: {timeItem._id}</p>
+                <p>Start: {timeItem.start_time}</p>
+                <p>Stop: {timeItem.stop_time}</p>
+                <p>Is Active: {timeItem.is_active.toString()}</p>
+            </div>
+        )}
+      </div>
     </div>
   );
 }
