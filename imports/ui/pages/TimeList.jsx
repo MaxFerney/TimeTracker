15370be3a397b1b99c9b3c63c282447a1a31c36f
Michael Walters <waltersm8@winthrop.edu>
2021-02-15 15:24:32 -0500

    Semi styled list times

--- imports/ui/pages/TimeList.jsx
+++ imports/ui/pages/TimeList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Helmet} from "react-helmet";
+import moment from 'moment';
 import Footer from './../components/footer.jsx';
 import Header from './../components/header.jsx';
 import { TimesCollectionAccess } from './../../../lib/times.js';
@@ -11,17 +12,15 @@ export default class TimeList extends React.Component{
                    <title>Error</title>
                </Helmet>
                <Header />
-               <h1>TIME LIST WOOT!</h1>
-               <p>This means we couldn't find the page you were looking for</p>
                <div className="times">
+                 <ul>
                   { TimesCollectionAccess.find().fetch().map((timeItem) =>
-                    <div key={timeItem._id} className="time_item">
-                      <p>Time ID: {timeItem._id}</p>
-                      <p>Start: {timeItem.start_time}</p>
-                      <p>Stop: {timeItem.stop_time}</p>
-                      <p>Is Active: {timeItem.is_active.toString()}</p>
-                    </div>
+                    <li key={timeItem._id} className="time_item">
+                      <p> {start = moment(timeItem.start_time*1000).format('LT')} - {moment(timeItem.stop_time*1000).format('LT')}</p>
+                      <p>Placeholder Category</p>
+                    </li>
                   )}
+                  </ul>
                 </div>
                <Footer />
            </div>
