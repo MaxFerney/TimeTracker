62e0c48c8afeee6fba9df34dea518dd9d2ede31e
MaxFerney <maxferney@gmail.com>
2021-02-16 13:38:54 -0500

    time list by date

--- imports/ui/pages/TimeList.jsx
+++ imports/ui/pages/TimeList.jsx
@@ -5,29 +5,68 @@ import Footer from './../components/footer.jsx';
 import Header from './../components/header.jsx';
 import { TimesCollectionAccess } from './../../../lib/times.js';
 export default class TimeList extends React.Component{
-    render(){
-        return (
-           <div>
-               <Helmet>
-                   <title>Error</title>
-               </Helmet>
-               <Header />
-               <div className="times">
-                    { TimesCollectionAccess.find({},{sort: {start_time:-1}}).fetch().map((timeItem) =>
-                      <div key={timeItem._id} className="time_item">
-                        {console.log(moment(timeItem.start_time*1000).format("MMMM Do YYYY"))}
+  datesAvaliable(){
+    var dates = [];
+    TimesCollectionAccess.find({},{sort: {start_time:-1}}).fetch().map((timeItem) => {
+      var date=moment(timeItem.start_time*1000).format("MMMM Do YYYY");
+      console.log("DATE: "+date);
+      console.log("DATE IN DATES: "+dates.indexOf(date) > -1);
+      if(!(dates.indexOf(date) > -1)){
+        dates.push(date);
+      }
+    });
+    dates.forEach((item, i) => {
+      console.log(item);
+    });
 
-                        <p>{moment(timeItem.start_time*1000).format("MMMM Do YYYY")}</p>
+    return(
+      dates
+    );
+  }
+  renderTimeItem(timeItem){
+    return(
+      <div key={timeItem._id} class="timeDetails">
+        <p> {start = moment(timeItem.start_time*1000).format('LT')} - {moment(timeItem.stop_time*1000).format('LT')}</p>
+        <p>Placeholder Category</p>
+      </div>
+    );
+  }
+  renderTimesInDate(date){
+    const allTimesInDate = TimesCollectionAccess.find({},{sort: {start_time:-1}}).fetch().map((timeItem) =>{
+      var currentDate = moment(timeItem.start_time*1000).format("MMMM Do YYYY");
+      if (currentDate == date){
+        return(this.renderTimeItem(timeItem));
+      }
+    });
+    return(
+      allTimesInDate
+    );
+  }
+  renderTimeList(){
+    var dates = this.datesAvaliable();
+    const dateList = dates.map((date, index) =>
+      <div key={index.toString()} class="time_item">
+        <p>{date}</p>
+        {this.renderTimesInDate(date)}
+      </div>
+    );
+    return (
+      dateList
+    );
+  }
 
-                        <div class="timeDetails">
-                          <p> {start = moment(timeItem.start_time*1000).format('LT')} - {moment(timeItem.stop_time*1000).format('LT')}</p>
-                          <p>Placeholder Category</p>
-                        </div>
-                      </div>
-                    )}
-                </div>
-               <Footer />
-           </div>
-        );
-    }
+  render(){
+    return (
+       <div>
+         <Helmet>
+           <title>Error</title>
+         </Helmet>
+         <Header />
+         <div className="times">
+            { this.renderTimeList() }
+         </div>
+         <Footer />
+       </div>
+    );
+  }
 }
