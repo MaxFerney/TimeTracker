afa2b5e15815939c40329e2b669ad3c9acf80c3e
MaxFerney <maxferney@gmail.com>
2021-02-16 16:31:12 -0500

    Merge branch 'main' of https://gith.com/MaxFerney/TimeTracker into main

--- imports/ui/pages/EditTime.jsx
+++ imports/ui/pages/EditTime.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import { useParams } from 'react-router-dom';
 
+import { useParams } from "react-router";
+
 import Footer from './../components/footer.jsx';
 import Header from './../components/header.jsx';
 
@@ -14,6 +16,7 @@ import { TimesCollectionAccess } from './../../../lib/times.js';
 //   console.log(id);
 // }
 export default class EditTime extends React.Component{
+<<<<<<< HEAD
 
   render(){
     return (
@@ -53,3 +56,26 @@ export default class EditTime extends React.Component{
 // <p>Start Time: {moment(this.props.timeItem.start_time*1000).format('LT')}</p>
 // <p>Stop Time: {moment(this.props.timeItem.stop_time*1000).format('LT')}</p>
 // <p>Date: {moment(this.props.timeItem.stop_time*1000).format("MMMM Do YYYY")}</p>
+=======
+    render(){
+        return (
+           <div>
+                <h1>Passed ID is this <br /> {this.props.passedID}</h1>
+                <Helmet>
+                    <title>Edit Time</title>
+                </Helmet>
+                <Header />
+                <h1>Edit</h1>
+
+                <p>Edit page for times</p>
+                <Footer />
+           </div>
+        );
+    }
+}
+/*
+EditTime.propTypes = {
+    timeItem: PropTypes.object.isRequired,
+};
+*/
+>>>>>>> e32f149be96595dc22c58961c0318e340d7ffdc3
