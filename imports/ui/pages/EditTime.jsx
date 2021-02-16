b40e50f0d94ac0f73917f3622ab9bed4c65c6160
Michael Walters <waltersm8@winthrop.edu>
2021-02-16 16:33:23 -0500

    Merge funky wunky is no longer funky wunky

--- imports/ui/pages/EditTime.jsx
+++ imports/ui/pages/EditTime.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {Helmet} from "react-helmet";
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 
 import { useParams } from "react-router";
 
@@ -16,47 +16,6 @@ import { TimesCollectionAccess } from './../../../lib/times.js';
 //   console.log(id);
 // }
 export default class EditTime extends React.Component{
-<<<<<<< HEAD
-
-  render(){
-    return (
-     <div>
-       <Helmet>
-           <title>Edit Time</title>
-       </Helmet>
-       <Header />
-       <h1>EDIT YOUR TIMES</h1>
-       <p>This means we couldn't find the page you were looking for</p>
-       <div class="timeInfo">
-       {console.log(this.props.passedTimeItem._id)}
-
-
-       </div>
-       <Footer />
-     </div>
-    );
-  }
-}
-// const time_id = () => {
-//   const [state, setState] = useState({});
-//   const { id } = useParams();
-//   useEffect( () => {
-//       api.getTeam(id).then(data => {
-//          //logic
-//       }
-// };
-// export default EditTime;
-
-
-// EditTime.propTypes = {
-//     timeItem: PropTypes.object.isRequired,
-// };
-
-// <h4>{this.props.timeItem._id}</h4>
-// <p>Start Time: {moment(this.props.timeItem.start_time*1000).format('LT')}</p>
-// <p>Stop Time: {moment(this.props.timeItem.stop_time*1000).format('LT')}</p>
-// <p>Date: {moment(this.props.timeItem.stop_time*1000).format("MMMM Do YYYY")}</p>
-=======
     render(){
         return (
            <div>
@@ -78,4 +37,3 @@ EditTime.propTypes = {
     timeItem: PropTypes.object.isRequired,
 };
 */
->>>>>>> e32f149be96595dc22c58961c0318e340d7ffdc3
--- imports/ui/pages/TimeList.jsx
+++ imports/ui/pages/TimeList.jsx
@@ -8,31 +8,21 @@ import Footer from './../components/footer.jsx';
 import Header from './../components/header.jsx';
 
 import { TimesCollectionAccess } from './../../../lib/times.js';
-<<<<<<< HEAD
 
-=======
-import { NavLink } from 'react-router-dom';
->>>>>>> e32f149be96595dc22c58961c0318e340d7ffdc3
 export default class TimeList extends React.Component{
   datesAvaliable(){
     var dates = [];
     TimesCollectionAccess.find({},{sort: {start_time:-1}}).fetch().map((timeItem) => {
       var date=moment(timeItem.start_time*1000).format("MMMM Do YYYY");
-<<<<<<< HEAD
-=======
       //console.log("DATE: "+date);
       //console.log("DATE IN DATES: "+dates.indexOf(date) > -1);
->>>>>>> e32f149be96595dc22c58961c0318e340d7ffdc3
       if(!(dates.indexOf(date) > -1)){
         dates.push(date);
       }
     });
-<<<<<<< HEAD
-=======
     dates.forEach((item, i) => {
       //console.log(item);
     });
->>>>>>> e32f149be96595dc22c58961c0318e340d7ffdc3
 
     return(
       dates
@@ -40,22 +30,11 @@ export default class TimeList extends React.Component{
   }
   renderTimeItem(timeItem){
     return(
-<<<<<<< HEAD
-      <div key={timeItem._id} class="timeDetails">
-        <NavLink to={"/edit/"+timeItem.start_time}>
-          <div>
-            <p> {start = moment(timeItem.start_time*1000).format('LT')} - {moment(timeItem.stop_time*1000).format('LT')}</p>
-            <p>Placeholder Category</p>
-          </div>
-        </NavLink>
-        {console.log("/edit/"+timeItem.start_time)}
-=======
       <div key={timeItem._id} className="timeDetails">
         <NavLink to={"/edit/" + timeItem._id}>
           <p>{start = moment(timeItem.start_time*1000).format('LT')} - {moment(timeItem.stop_time*1000).format('LT')}</p>
           <p>Placeholder Category</p>
         </NavLink>
->>>>>>> e32f149be96595dc22c58961c0318e340d7ffdc3
       </div>
     );
   }
